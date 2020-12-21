f2df4672ba1026a82f0065347e330f01eb0cd2b6
Raiza Escobar <raizaescobar@gmail.com>
2020-12-21 14:09:30 +0100

    about me

--- src/Components/CoverSection.js
+++ src/Components/CoverSection.js
@@ -6,7 +6,7 @@ export default function CoverSection() {
     return (
         <div className="cover-container" id="start">
         <Video src="/videos/mountain.mov" className="video" style={{position:"fixed"}} autoPlay loop muted/>
-        <h1>Hello, I'm Raiza Escobar,</h1>
+        <h1>Hello, I'm Raiza Escobar</h1>
         <p> a web developer with passion for learning</p>
         </div>
     )
