113f7a70c7c7d9737771281e993dae32007d821e
Raiza Escobar <raizaescobar@gmail.com>
2020-12-22 08:52:40 +0100

    grammar

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -13,7 +13,7 @@ export default function Aboutme() {
                 <p> 
         
                 
-Hi, I am Raiza, a foodie, mountains lover, books lover and of course code lover.<br/><br/>
+Hi, I'm Raiza, a foodie, mountains lover, books lover and of course a code lover.<br/><br/>
 
 I am from Venezuela, but living in Barcelona since 2009. The last 6 years I have been working in the financial sector, especially in the banking sector, 
 
@@ -22,7 +22,7 @@ which has allowed me to develop new skills, such as working under pressure, busi
 Last year, in the midst of the worst world crisis of the last decades, I had my own existential crisis, so I decided to stop for a moment and think about how I saw myself in the following years, 
 what I saw myself doing the rest of my life and the answer was...programming.
 
-I love programming, feeling like every minute of time I spend studying is an investment in myself and my code; I learn fast and I love seeing results, that's something that programming gives me. 
+I love programming, feeling like every minute of time I spend studying, is an investment in myself and my code; I learn fast and I love seeing results, that's something that programming gives me. 
 
 So the logical but not easy next step was to quit my job and find the best way to become a programmer.<br/><br/>
 
